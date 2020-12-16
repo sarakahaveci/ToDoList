93ae35d2cb8d2f7949cf4a966c8cc44067316cfc
yamoonz <yamooonz@gmail.com>
2020-12-16 18:56:44 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -11,78 +11,78 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  state = {
-    todos: []
-  };
+    state = {
+        todos: []
+    };
 
-  componentDidMount() {
-    axios
-      .get('http://jsonplaceholder.typicode.com/todos?_limit=10')
-      .then(res => this.setState({ todos: res.data }));
-  }
+    componentDidMount() {
+        axios
+            .get('http://jsonplaceholder.typicode.com/todos?_limit=10')
+            .then(res => this.setState({todos: res.data}));
+    }
 
-  // Toggle Complete
-  markComplete = id => {
-    this.setState({
-      todos: this.state.todos.map(todo => {
-        if (todo.id === id) {
-          todo.completed = !todo.completed;
-        }
-        return todo;
-      })
-    });
-  };
+    // Toggle Complete
+    markComplete = id => {
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed;
+                }
+                return todo;
+            })
+        });
+    };
 
-  // Delete Todo
-  delTodo = id => {
-    axios.delete(`http://jsonplaceholder.typicode.com/todos/${id}`).then(res =>
-      this.setState({
-        todos: [...this.state.todos.filter(todo => todo.id !== id)]
-      })
-    );
-  };
+    // Delete Todo
+    delTodo = id => {
+        axios.delete(`http://jsonplaceholder.typicode.com/todos/${id}`).then(res =>
+            this.setState({
+                todos: [...this.state.todos.filter(todo => todo.id !== id)]
+            })
+        );
+    };
 
-  // Add Todo
-  addTodo = title => {
-    axios
-      .post('http://jsonplaceholder.typicode.com/todos', {
-        title,
-        completed: false
-      })
-      .then(res => {
-        res.data.id = uuid();
-        this.setState({ todos: [...this.state.todos, res.data] });
-      });
-  };
+    // Add Todo
+    addTodo = title => {
+        axios
+            .post('http://jsonplaceholder.typicode.com/todos', {
+                title,
+                completed: false,
+            })
+            .then(res => {
+                res.data.id = uuid();
+                this.setState({ todos: [...this.state.todos, res.data] });
+            });
+    };
 
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <div className="container">
-            <Header />
-            <Route
-              exact
-              path="/"
-              render={props => (
-                <React.Fragment>
-                  <AddTodo addTodo={this.addTodo} />
-                  <Todos
-                    todos={this.state.todos}
-                    markComplete={this.markComplete}
-                    delTodo={this.delTodo}
-                  />
-                </React.Fragment>
-              )}
-            />
-            <Route path="/about" component={About} />
-          </div>
-        </div>
-        <Footer/>
-      </Router>
-     
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <div className="container">
+                        <Header />
+                        <Route
+                            exact
+                            path="/"
+                            render={props => (
+                                <React.Fragment>
+                                        <AddTodo addTodo={this.addTodo} />
+                                        <Todos
+                                            todos={this.state.todos}
+                                            markComplete={this.markComplete}
+                                            delTodo={this.delTodo}
+                                    />
+                                </React.Fragment>
+                            )}
+                        />
+                        <Route path="/about" component={About} />
+                    </div>
+                </div>
+                <Footer/>
+            </Router>
+
+        );
+    }
 }
 
 export default App;
