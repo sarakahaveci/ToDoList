8e55916dd042ba621ace37f82ceb8687b85f1cfc
yamoonz <yamooonz@gmail.com>
2021-12-14 15:42:17 +0300

    edited home page

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <header style={headerStyle}>
-      <h1>To Watch List</h1>
+      <h1>To Do List</h1>
       <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
     </header>
   )
